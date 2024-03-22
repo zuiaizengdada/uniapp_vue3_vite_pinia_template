95b23865a77722ee266325b1d7e539e1074dcfb2
曾宇奇 <accounts_65dc45ba2094afdeeff81b90@mail.teambition.com>
2024-03-22 12:13:52 +0800

    更新 global.d.ts 更新请求返回格式

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,6 +1,27 @@
 // 网络请求返回格式
 export type Data<T> = {
-  code: string
-  msg: string
-  result: T
+  // 状态码
+  code?: number
+  // 响应对象
+  data: T
+  // 状态描述
+  message?: string
+  // 是否成功
+  success?: boolean
+  // 时间戳
+  timestamp?: number
+  // 分布式链路id
+  traceId?: string
+}
+
+// UniStorage请求方法
+export interface UniStorageMethods {
+  get: (key: string) => any
+  getSync: (key: string) => any
+  set: (params: { key: string; data: any; success?: () => void; fail?: (error: Error) => void }) => void
+  setSync: (key: string, data: any) => void
+  remove: (key: string) => void
+  removeSync: (key: string) => void
+  clear: () => void
+  clearSync: () => void
 }
