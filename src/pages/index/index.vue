b13ead7ec1f97c7f2145f3da7b47974a1aa155ae
zengyuqi <1203049946@qq.com>
2024-07-08 14:41:06 +0800

    ci: 新增项目国际化示例

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,15 +1,34 @@
 <script setup lang="ts">
+import i18n from '@/locale'
 const { userName, setUserName } = useStore('user')
+
+const t = i18n.global.t
+
+type Languages = 'zh' | 'en'
+function handleChangeLanguageTap(language: Languages) {
+  i18n.global.locale = language
+}
 </script>
 
 <template>
   <view class="index">
     <view class="name">
-      {{ userName }}
+      <text>
+        {{ userName }}
+      </text>
     </view>
 
-    <view class="btn">
+    <view class="btn-group">
       <button @tap="setUserName('zengdada1')">修改名字</button>
+    </view>
+
+    <view class="i18n">
+      <text>{{ t('demo') }}</text>
+    </view>
+
+    <view class="btn-group">
+      <button @tap="handleChangeLanguageTap('zh')">中文</button>
+      <button @tap="handleChangeLanguageTap('en')">English</button>
     </view>
   </view>
 </template>
@@ -22,14 +41,11 @@ const { userName, setUserName } = useStore('user')
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  gap: 10px;
 
   .name {
     width: 100%;
     text-align: center;
-  }
-
-  .btn {
-    margin-top: 10px;
   }
 }
 </style>
