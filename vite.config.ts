416f9729c5705cf35105c78f0b03f66a203708e7
zengyuqi <1203049946@qq.com>
2024-08-12 15:04:32 +0800

    ci: 添加@vue-macros/reactivity-transform依赖并更新相关配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 import PiniaAutoRefs from 'pinia-auto-refs'
 import tailwindcss from 'tailwindcss'
 import uniTailwind from '@uni-helper/vite-plugin-uni-tailwind'
+import ReactivityTransform from '@vue-macros/reactivity-transform/vite'
 import { resolve } from 'path'
 
 export default defineConfig({
@@ -31,6 +32,7 @@ export default defineConfig({
       //为true时在项目根目录自动创建
       dts: 'src/types/auto-import.d.ts'
     }),
+    ReactivityTransform(),
     PiniaAutoRefs({
       outputFile: 'src/utils/modules/pinia-auto-refs.ts'
     })
