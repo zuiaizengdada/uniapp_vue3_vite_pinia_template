880ca59e65a918c1948ac5a449cb2ef7ee52705b
zengyuqi <1203049946@qq.com>
2024-09-05 19:22:57 +0800

    ci: 新增vitest模块

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 import AutoImport from 'unplugin-auto-import/vite'
@@ -6,7 +8,6 @@ import tailwindcss from 'tailwindcss'
 import uniTailwind from '@uni-helper/vite-plugin-uni-tailwind'
 import ReactivityTransform from '@vue-macros/reactivity-transform/vite'
 import { resolve } from 'path'
-
 export default defineConfig({
   resolve: {
     alias: [
@@ -25,11 +26,11 @@ export default defineConfig({
         'vue',
         'pinia',
         'uni-app',
+        'vitest',
         {
           '@/utils/modules/pinia-auto-refs': ['useStore']
         }
       ],
-      //为true时在项目根目录自动创建
       dts: 'src/types/auto-import.d.ts'
     }),
     ReactivityTransform(),
