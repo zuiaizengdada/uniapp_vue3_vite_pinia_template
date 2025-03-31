a2e4f96e44f10339c7a044a95be82c0d53396880
zengyuqi <1203049946@qq.com>
2025-03-31 11:40:26 +0800

    chore: 更新依赖项，添加 @tanstack/vue-query 插件并配置 QueryClient

--- src/common/register/modules/RegisterApp.ts
+++ src/common/register/modules/RegisterApp.ts
@@ -1,17 +1,23 @@
 import '@/common/register/modules/RegisterImport'
 import { RegisterProperties } from './RegisterProperties'
 import { App, FunctionPlugin } from 'vue'
+
 // 国际化
 import i18n from '@/locale'
 
 // pinia
 import store, { Pinia } from '@/store'
 
+import { QueryClient, VueQueryPlugin } from '@tanstack/vue-query'
+// 创建 QueryClient 实例
+const queryClient = new QueryClient()
+
 export function RegisterApp(app: App) {
   RegisterProperties(app)
 
   app.use(store as unknown as FunctionPlugin<[]>)
   app.use(i18n as unknown as FunctionPlugin<[]>)
+  app.use(VueQueryPlugin, { queryClient })
 
   return {
     app,
