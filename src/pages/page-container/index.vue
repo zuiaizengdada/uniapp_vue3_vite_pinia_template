5bd4f30533c6455c987d8c95c36c6a8d9fdb6442
zengyuqi <1203049946@qq.com>
2025-03-31 10:06:53 +0800

    refactor: 优化页面容器模板结构，使用view和block替代template，提升可读性

--- src/pages/page-container/index.vue
+++ src/pages/page-container/index.vue
@@ -11,21 +11,23 @@ const { isFirstEnterApp } = useStore('system')
 </script>
 
 <template>
-  <WelcomVideoComponent v-if="isFirstEnterApp" />
+  <view>
+    <WelcomVideoComponent v-if="isFirstEnterApp" />
 
-  <template v-else>
-    <view class="flex relative flex-col h-screen">
-      <view class="overflow-hidden relative flex-1 pointer-events-none" @touchstart="handleTouchStart" @touchmove="handleTouchMove" @touchend="handleTouchEnd">
-        <PageIndexComponent class="absolute inset-0" :class="getPageClass(0)" :style="getPageStyle(0)" v-show="getPageShowCondition(0)" :tab-index="tabIndex" />
+    <block v-else>
+      <view class="flex relative flex-col h-screen">
+        <view class="overflow-hidden relative flex-1 pointer-events-none" @touchstart="handleTouchStart" @touchmove="handleTouchMove" @touchend="handleTouchEnd">
+          <PageIndexComponent class="absolute inset-0" :class="getPageClass(0)" :style="getPageStyle(0)" v-show="getPageShowCondition(0)" :tab-index="tabIndex" />
 
-        <PageWebsocketComponent class="absolute inset-0" :class="getPageClass(1)" :style="getPageStyle(1)" v-show="getPageShowCondition(1)" :tab-index="tabIndex" />
+          <PageWebsocketComponent class="absolute inset-0" :class="getPageClass(1)" :style="getPageStyle(1)" v-show="getPageShowCondition(1)" :tab-index="tabIndex" />
 
-        <PageMittComponent class="absolute inset-0" :class="getPageClass(2)" :style="getPageStyle(2)" v-show="getPageShowCondition(2)" :tab-index="tabIndex" />
+          <PageMittComponent class="absolute inset-0" :class="getPageClass(2)" :style="getPageStyle(2)" v-show="getPageShowCondition(2)" :tab-index="tabIndex" />
 
-        <PageScrollComponent class="absolute inset-0" :class="getPageClass(3)" :style="getPageStyle(3)" v-show="getPageShowCondition(3)" :tab-index="tabIndex" />
+          <PageScrollComponent class="absolute inset-0" :class="getPageClass(3)" :style="getPageStyle(3)" v-show="getPageShowCondition(3)" :tab-index="tabIndex" />
+        </view>
       </view>
-    </view>
 
-    <AppTabbar :selected="tabIndex" @change="handleTabChange" />
-  </template>
+      <AppTabbar :selected="tabIndex" @change="handleTabChange" />
+    </block>
+  </view>
 </template>
